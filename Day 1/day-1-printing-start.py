902f5da0c995ff43a6333a0e625267c419975f60
Rana Pratik <ranapratik1612@gmail.com>
2023-01-01 22:36:50 +0530

    Day 1

---
+++ Day 1/day-1-printing-start.py
@@ -0,0 +1,7 @@
+# Write your code below this line 👇
+# print("Hello Python")
+# print("Hello World\nHello World\nHello World")  
+print("Hello" + " Python")
+
+
+print("Hello " + input("What is your name:"))
---
+++ Day 1/day-1-variables-start.py
@@ -0,0 +1,7 @@
+# name="pratik";
+# print(name);
+# name="Rohan";
+# print(name);
+name=input("What is your name?")
+length=len(name);
+print(length);
