4300099220c6ad253deb9b7a47f171325c3e9546
Rana Pratik <ranapratik1612@gmail.com>
2023-01-17 11:10:14 +0530

    Day 16 update

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -2,18 +2,22 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-menu=Menu()
-coffeemaker=CoffeeMaker()
-menuitem=MenuItem()
-isrunning=True
-while isrunning:
-    menuitem.name=input(f"What would you like? {menu.get_items()}: ")
-    if(menuitem=="report"):
-        print(coffeemaker.report())
-    elif(menuitem=="off"):
-        isrunning=False
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+# drink=MenuItem()
+is_running = True
+
+while is_running:
+    choice = input(f"What would you like? {menu.get_items()}: ")
+    if (choice == 'report'):
+        coffee_maker.report()
+        money_machine.report()
+    elif (choice == 'off'):
+        is_running = False
     else:
-        if(coffeemaker.is_resource_sufficient(menuitem.name)):
-            make_payment()
-        else:
-            print("Resources are insufficient")
+        drink = menu.find_drink(choice)
+        # print(drink.ingredients)
+        # print(drink.name,drink.cost)
+        if (coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost)):
+                coffee_maker.make_coffee(drink)
