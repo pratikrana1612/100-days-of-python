9e6194fbcfdc05b676d71a58a3be28bcffc1eb26
Rana Pratik <ranapratik1612@gmail.com>
2023-01-20 22:05:43 +0530

    Day 20

--- Day 20/Sanke-Game/main.py
+++ Day 20/Sanke-Game/main.py
@@ -7,12 +7,21 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title("My Snake Game")
+screen.tracer(0)
+snake = Snake()
+screen.update()
 
-snake=Snake()
-# timmy = Turtle
+screen.listen()
+screen.onkey(snake.snake_up, "Up")
+screen.onkey(snake.snake_down, "Down")
+screen.onkey(snake.snake_left, "Left")
+screen.onkey(snake.snake_right, "Right")
+# timmy = Turtle()
 game_is_on = True
 while game_is_on:
     snake.move()
+    time.sleep(0.1)
+    screen.update()
     # turtles[0].left(90)
 
 screen.exitonclick()
--- Day 20/Sanke-Game/snake.py
+++ Day 20/Sanke-Game/snake.py
@@ -1,11 +1,17 @@
 from turtle import Screen, Turtle
-import time
 
-screen=Screen()
-screen.tracer(0)
+DISTANCE_COVER = 20
+UP=90
+DOWN=270
+LEFT=180
+RIGHT=0
+
 class Snake:
     def __init__(self):
         self.turtles = []
+        self.create()
+
+    def create(self):
         x_cordinate = 0
         for _ in range(3):
             new_turtle = Turtle('square')
@@ -14,14 +20,27 @@ class Snake:
             new_turtle.goto(x_cordinate, 0)
             self.turtles.append(new_turtle)
             x_cordinate -= 20
-    screen.update()
 
     def move(self):
-        for i in range(len(self.turtles)-1,0,-1):
-                # turtle=self.turtles[1]
-                position=self.turtles[i-1].pos()
-                self.turtles[i].goto(position)
-                # screen.update()
-        self.turtles[0].forward(20)
-        time.sleep(0.1)
-        screen.update() 
+        for i in range(len(self.turtles)-1, 0, -1):
+            # turtle=self.turtles[1]
+            position = self.turtles[i-1].pos()
+            self.turtles[i].goto(position)
+            # screen.update()
+        self.turtles[0].forward(DISTANCE_COVER)
+
+    def snake_up(self):
+        if self.turtles[0].heading()!=DOWN:
+            self.turtles[0].setheading(90)
+
+    def snake_down(self):
+        if self.turtles[0].heading()!=UP:
+            self.turtles[0].setheading(270)
+
+    def snake_left(self):
+        if self.turtles[0].heading()!=RIGHT:
+            self.turtles[0].setheading(180)
+
+    def snake_right(self):
+        if self.turtles[0].heading()!=RIGHT:
+            self.turtles[0].setheading(0)
