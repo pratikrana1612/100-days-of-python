2eb173ec7e2f15de9542deba979c1c11a12dddeb
Rana Pratik <ranapratik1612@gmail.com>
2023-02-05 23:47:28 +0530

    Day 36

--- Day 32/Birthday wisher project/main.py
+++ Day 32/Birthday wisher project/main.py
@@ -9,5 +9,36 @@
 # 4. Send the letter generated in step 3 to that person's email address.
 
 
+import pandas
+import datetime as dt
+from random import choice
+import smtplib
 
+email='std122021@gmail.com'
+password="rqyvxrgfnqtuyoyx"
 
+now = dt.datetime.now()
+letters = []
+with open('letter_templates/letter_1.txt') as file:
+    letter = file.read()
+    letters.append(letter)
+with open('letter_templates/letter_2.txt') as file:
+    letter = file.read()
+    letters.append(letter)
+with open('letter_templates/letter_2.txt') as file:
+    letter = file.read()
+    letters.append(letter)
+# for letter in letters:
+#     print(letter)
+# print(letters)
+data = pandas.read_csv('birthdays.csv')
+for (index, row) in data.iterrows():
+    if (now.month == row['month'] and now.day == row['day']):
+        # print("it's your birthday")
+        letter = choice(letters)
+        letter=letter.replace('[NAME]',row['name']).replace('Angela',"Pratik")
+        with smtplib.SMTP('smtp.gmail.com',port=587) as connection:
+            connection.starttls()
+            connection.login(user=email,password=password)
+            connection.sendmail(from_addr=email,to_addrs='ranapratik1612@gmail.com',msg=f'Subject:Birthday Wish\n\n{letter}')
+        # letter.replace('Angela','afad')
