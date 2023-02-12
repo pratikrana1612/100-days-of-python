f264421c2aa2bd0b5b01cac97ab8ae5a4d666df0
Rana Pratik <ranapratik1612@gmail.com>
2023-02-12 23:18:22 +0530

    Day 43 put,delete request and strftime

--- Day 37/Habit Tracker/main.py
+++ Day 37/Habit Tracker/main.py
@@ -1,8 +1,9 @@
+import datetime as dt
 import requests
 
 
-USERNAME="pratik16"
-TOKEN="fadk;fadlfkasdfa"
+USERNAME = "pratik16"
+TOKEN = "fadk;fadlfkasdfa"
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
     "token": "fadk;fadlfkasdfa",
@@ -14,16 +15,29 @@ user_params = {
 # print(response.text)
 
 
-graph_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs"
-graph_cofing={
-    "id":"graph1",
-    "name":"cycling graph",
-    "unit":"km",
-    "type":"float",
-    "color":"ajisai"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_cofing = {
+    "id": "graph1",
+    "name": "cycling graph",
+    "unit": "km",
+    "type": "float",
+    "color": "ajisai"
 }
-headers={
-    "X-USER-TOKEN":TOKEN
+headers = {
+    "X-USER-TOKEN": TOKEN
 }
-response=requests.post(url=graph_endpoint,json=graph_cofing,headers=headers)
+# response=requests.post(url=graph_endpoint,json=graph_cofing,headers=headers)
+# print(response.text)
+# now = dt.datetime(year=2023,month=2,day=1)
+now = dt.datetime(year=2023,month=2,day=1)
+# month =  "0"+ str(now.month) if now.month < 9 else now.month
+# day =  "0"+ str(now.day) if now.day < 9 else now.day
+endpoint_post_pixel = f'{pixela_endpoint}/{USERNAME}/graphs/graph1'
+post_pixel_json = {
+    "date":now.strftime("%Y%m%d"),
+    "quantity": input("How many Kilo meters did you cycle today?")
+}
+response = requests.post(
+    endpoint_post_pixel, json=post_pixel_json, headers=headers)
+# response=requests.delete(endpoint_post_pixel,json=post_pixel_json,headers=headers)
 print(response.text)
